a4642eb3f44430f3521322b5ad0991feef3fd0bc
leader-akash <117633377+leader-akash@users.noreply.github.com>
2023-06-10 13:17:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+window.addEventListener('keydown', playSound);
+
+function playSound(event) {
+  const key = document.querySelector(`div[data-key="${event.keyCode}"]`);
+  const sound = key.querySelector('.sound');
+  if (!key) return;
+
+  // Add CSS class for visual feedback
+  key.classList.add('playing');
+
+  // Play audio
+  // Replace the sound logic with your own audio files or API calls
+  sound.innerHTML = 'Playing...';
+
+  // Remove CSS class after the transition ends
+  key.addEventListener('transitionend', function() {
+    key.classList.remove('playing');
+  });
+}
